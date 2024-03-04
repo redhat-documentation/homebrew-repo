b9ff227d0848f9424546bad3ef02cff1074ec83f
Marek Suchánek <msuchane@redhat.com>
2024-03-04 12:17:32 +0100

    Update near-facsimile to 1.0.8

--- Formula/near-facsimile.rb
+++ Formula/near-facsimile.rb
@@ -1,8 +1,8 @@
 class NearFacsimile < Formula
   desc "Find similar or identical text files in a directory"
   homepage "https://github.com/msuchane/near-facsimile"
-  url "https://github.com/msuchane/near-facsimile/archive/refs/tags/v1.0.7.tar.gz"
-  sha256 "2ea90c8a35c808d5414f2dcad1eb70218fc89af14bcd52bacce05e76b95b994d"
+  url "https://github.com/msuchane/near-facsimile/archive/refs/tags/v1.0.8.tar.gz"
+  sha256 "91ec7c623ef7502ca399a088af85cb94ec06301c2b851d293afdbaa49d2143bc"
   license "Apache-2.0"
   head "https://github.com/msuchane/near-facsimile.git", branch: "main"
 
