a7f76b1143b985bbadac8fbcb650042f414a5f6c
Marc Muehlfeld <mmuehlfeld@redhat.com>
2025-05-27 15:31:29 +0200

    Update acorns to 1.0.1

--- Formula/acorns.rb
+++ Formula/acorns.rb
@@ -1,8 +1,8 @@
 class Acorns < Formula
   desc "Generate an AsciiDoc release notes document from tracking tickets. "
   homepage "https://github.com/redhat-documentation/homebrew-repo"
-  url "https://github.com/redhat-documentation/acorns/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "05bb1263291376648ac0b09fcc0d83c63259b5fa8322c7f927e1d0c3f0c49a95"
+  url "https://github.com/redhat-documentation/acorns/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "92b8e5b2297404525814071665c27ea288b5a5f1d8fc3b55fbdc8f42606cb293"
   license "GPL-3.0-or-later"
   head "https://github.com/redhat-documentation/acorns.git", branch: "main"
 
