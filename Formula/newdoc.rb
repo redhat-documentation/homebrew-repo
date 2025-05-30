8be10dd263c6e45c9f1c931a7a6dfff96b5b1eb6
Marc Muehlfeld <mmuehlfeld@redhat.com>
2025-05-30 08:57:53 +0200

    Update newdoc to 2.18.5

--- Formula/newdoc.rb
+++ Formula/newdoc.rb
@@ -1,8 +1,8 @@
 class Newdoc < Formula
   desc "The newdoc tool generates files formatted with AsciiDoc, which are used in Red Hat documentation."
   homepage "https://github.com/redhat-documentation/newdoc"
-  url "https://github.com/redhat-documentation/newdoc/archive/refs/tags/v2.18.4.tar.gz"
-  sha256 "7f8fcc2a7429b067b2d3d120cebcfec526661266c8acffdc5566c2b99b2c653a"
+  url "https://github.com/redhat-documentation/newdoc/archive/refs/tags/v2.18.5.tar.gz"
+  sha256 "7a5a0944c83d4c7d589ce001fe6f18eaee8ed38b79ade9b9b66b5a97b28e09de"
   license "GPL-3.0-or-later"
   head "https://github.com/redhat-documentation/newdoc.git", branch: "main"
 
